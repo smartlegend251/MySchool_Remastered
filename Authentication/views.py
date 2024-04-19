4f37cdfeb4ef46a60bfb2b8b760ecc3566302da3
smartlegend251 <smartlegend2001@gmail.com>
2024-04-19 22:37:26 +0530

    MySchool V 1.1

--- Authentication/views.py
+++ Authentication/views.py
@@ -6,7 +6,17 @@ def student_login(request):
         pass
     return render(request, 'student/student_login.html')
 
+def student_register(request):
+    if request.method == "POST":
+        pass
+    return render(request, 'student/student_register.html')
+
 def staff_login(request):
     if request.method == "POST":
         pass
     return render(request, 'staff/staff_login.html')
+
+def staff_register(request):
+    if request.method == "POST":
+        pass
+    return render(request, 'staff/staff_register.html')
